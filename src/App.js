4fbb08edc6430ac309904ffe04f106b5c5c85eeb
hridayroy19 <hrhridoyroy503@gmail.com>
2024-04-02 13:18:50 +0600

    crat a navabar and setup font awsome

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Card from "./Card";
+import Navbar from "./Navbar";
+
+
+function App() {
+  return (
+    <div>
+<Navbar></Navbar>
+<Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
+const Card = () => {
+    return (
+        <div className='mt-10  flex justify-center'>
+        <div className="card w-[500px] border-2 bg-white shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Card;
